17e9c4d67ac0cc6a50c53bdb33890c8172e831eb
suganya-web <suganya.donzz508@gmail.com>
2025-03-22 14:39:56 +0530

    first commit

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,11 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import {createBrowserRouter,RouterProvider} from 'react-router-dom'
+import {HashRouter,Routes,Route} from 'react-router-dom'
 import Viewstory from './Viewstory.jsx'
 import Profile from './Profile.jsx'
 
-const router=createBrowserRouter(
+/*const router=createBrowserRouter(
   [
     {
       path:'/',
@@ -21,7 +21,14 @@ const router=createBrowserRouter(
       element:<Profile/>
     }
   ]
-)
+)*/
 createRoot(document.getElementById('root')).render(
-  <RouterProvider router={router}/>
-)
+  //<RouterProvider router={router}/>
+  <HashRouter>
+    <Routes>
+      <Route path='/' element={<App />} />
+      <Route path='/story/:id/:tot' element={<Viewstory />} />
+      <Route path='/profile' element={<Profile />} />
+    </Routes>
+  </HashRouter>
+);
